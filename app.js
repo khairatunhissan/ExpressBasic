df134882e9f69a1cb82afd9f472e8955bc84c9bd
Khairatun Hissan <khairatunhissan@gmail.com>
2021-06-30 23:02:18 +0600

    Model created and mongodb connected

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+require('dotenv').config()
 const express=require('express')
 const app=express()
 const session=require('express-session')
 const flash =require('connect-flash')
 const indexRoutes=require('./routes/index.routes')
 const userRoutes=require('./routes/users.routes')
+const mongoose=require('mongoose')
+
+//connect to db
+mongoose.connect(process.env.MongoURI,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+console.log('Mongodb Connected')
+}).catch(()=>{
+    console.log(error)
+})
 
 app.use(express.static('public'))
 
