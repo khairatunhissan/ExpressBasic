08f785391b436b81d680439046f01b1b1bfe3b9f
Khairatun Hissan <khairatunhissan@gmail.com>
2021-06-30 23:25:13 +0600

    New user created and registration is done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,6 @@
+const User=require('../models/User.model')
+const bcrypt=require('bcryptjs')
+
 const getLogin=(req,res)=>{
     res.render('users/login.ejs')
 
@@ -17,10 +20,11 @@ const getRegister=(req,res)=>{
 
 const postRegister=(req,res)=>{
     const{name,email,password,confirm_password}=req.body
-    console.log(name)
+    /*console.log(name)
     console.log(email)
     console.log(password)
-   console.log(confirm_password)
+   console.log(confirm_password)*/
+  
    //Data validation
    const errors=[];
    if(!name||!email||!password||!confirm_password){
@@ -35,11 +39,50 @@ const postRegister=(req,res)=>{
    if(errors.length>0){
        req.flash('errors',errors)
        res.redirect('/users/register')
-   }else{
-       res.redirect('/users/login')
    }
-
-}
+   //Create New User
+   else{
+    User.findOne({ email: email }).then((user) => {
+        if (user) {
+          errors.push("User already exists with this email!");
+          req.flash("errors", errors);
+          res.redirect("/users/register");
+        } else {
+          bcrypt.genSalt(10, (err, salt) => {
+            if (err) {
+              errors.push(err);
+              req.flash("errors", errors);
+              res.redirect("/users/register");
+            } else {
+              bcrypt.hash(password, salt, (err, hash) => {
+                if (err) {
+                  errors.push(err);
+                  req.flash("errors", errors);
+                  res.redirect("/users/register");
+                } else {
+                  const newUser = new User({
+                    name,
+                    email,
+                    password: hash,
+                  });
+                  newUser
+                    .save()
+                    .then(() => {
+                      res.redirect("/users/login");
+                    })
+                    .catch(() => {
+                      errors.push("Saving User to the database failed!");
+                      req.flash("errors", errors);
+                      res.redirect("/users/register");
+                    });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+   }
 
 module.exports={
     postLogin,getLogin,postRegister,getRegister
