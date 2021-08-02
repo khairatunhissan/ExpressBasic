a6d40a44e34b7dd492abe19bee2afcb865e5f9e0
khairatunhissan <khairatunhissan@gmail.com>
2021-08-02 11:31:44 +0600

    Registration form done for Programming Contest

--- controllers/programmingContest.controller.js
+++ controllers/programmingContest.controller.js
@@ -6,6 +6,25 @@ const getPC=(req,res)=>{
 }
 
 const postPC=(req,res)=>{
+    const{teamname,institution,coachname,coachcontact,coachemail,coachtshirt,leadername,leadercontact,leaderemail,leadertshirt,member1name,member1contact,member1email,member1tshirt,member2name,member2contact,member2email,member2tshirt}=req.body
+    console.log(teamname)
+    console.log(institution)
+    console.log(coachname)
+    console.log(coachcontact)
+    console.log(coachemail)
+    console.log(coachtshirt)
+    console.log(leadername)
+    console.log(leadercontact)
+    console.log(leaderemail)
+    console.log(leadertshirt)
+    console.log(member1name)
+    console.log(member1contact)
+    console.log(member1email)
+    console.log(member1tshirt)
+    console.log(member2name)
+    console.log(member2contact)
+    console.log(member2email)
+    console.log(member2tshirt)
     res.render('programming-contest/register.ejs')
 }
 
